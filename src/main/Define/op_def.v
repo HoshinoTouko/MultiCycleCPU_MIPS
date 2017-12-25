b51bce26cf5ea6cbe025204b2602d66e1887d23b
HoshinoTouko <oranly@hotmail.com>
2017-12-25 10:33:49 +0800

    Change format of define var.

--- src/main/Define/op_def.v
+++ src/main/Define/op_def.v
@@ -1,38 +1,38 @@
 // R-Type
-`define R_OP 6'b000000
+`define OP_R 6'b000000
 
 // I-Type
 // TODO: Fix LBU, LH, LHU, SH, JALR
     // Memory
-`define LB_OP 6'100000
-`define LBU_OP 6'000000
-`define LH_OP 6'000000
-`define LHU_OP 6'000000
-`define LW_OP 6'100011
-`define SB_OP 6'101000
-`define SH_OP 6'000000
-`define SW_OP 6'101011
+`define OP_LB 6'100000
+`define OP_LBU 6'000000
+`define OP_LH 6'000000
+`define OP_LHU 6'000000
+`define OP_LW 6'100011
+`define OP_SB 6'101000
+`define OP_SH 6'000000
+`define OP_SW 6'101011
 
     // Imm calculate
-`define ADDI_OP 6'001000
-`define ADDIU_OP 6'001001
-`define ANDI_OP 6'001100
-`define ORI_OP 6'001101
-`define XORI_OP 6'001110
-`define LUI_OP 6'001111
-`define SLTI_OP 6'001010
-`define SLTIU_OP 6'001011
+`define OP_ADDI 6'001000
+`define OP_ADDIU 6'001001
+`define OP_ANDI 6'001100
+`define OP_ORI 6'001101
+`define OP_XORI 6'001110
+`define OP_LUI 6'001111
+`define OP_SLTI 6'001010
+`define OP_SLTIU 6'001011
 
 // Branch
-`define BEQ_OP 6'000100
-`define BNE_OP 6'000101
-`define BLEZ_OP 6'000110
-`define BGTZ_OP 6'000111
-`define BLTZ_OP 6'000001
-`define BGEZ_OP 6'000001
+`define OP_BEQ 6'000100
+`define OP_BNE 6'000101
+`define OP_BLEZ 6'000110
+`define OP_BGTZ 6'000111
+`define OP_BLTZ 6'000001
+`define OP_BGEZ 6'000001
 
 // J
-`define J_OP 6'000010
-`define JAL_OP 6'000011
-`define JALR_OP 6'000000
-`define JR_OP 6'000000
+`define OP_J 6'000010
+`define OP_JAL 6'000011
+`define OP_JALR 6'000000
+`define OP_JR 6'000000
